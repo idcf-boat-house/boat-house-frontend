b9f6a1586fd7bdd7feb70298fe2e3155e38eecb5
Wilson Bo <wilsonbo@leansoftx.com>
2020-02-05 13:30:41 +0800

    update jenkins files

--- pipelines/ci.groovy
+++ pipelines/ci.groovy
@@ -3,17 +3,17 @@ pipeline {
     stages {
         stage('Build') { 
             steps {
-                // 
+                sh 'echo hello world! Build!'
             }
         }
         stage('Test') { 
             steps {
-                // 
+                sh 'echo hello world! Test!'
             }
         }
         stage('Deploy') { 
             steps {
-                // 
+                sh 'echo hello world! Deploy!'
             }
         }
     }
