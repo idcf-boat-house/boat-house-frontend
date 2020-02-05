bdbe92955e3af828ae3eb6ce42c80734f1288f63
周文洋 <shuufumihiro@shuufumihiros-MacBook-Pro.local>
2020-02-05 15:07:12 +0800

    init stats service api

---
+++ statistics-service/api/server.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    async = require('async'),
+    pg = require("pg"),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+var port = process.env.PORT || 6000;
+
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Welcome BoatHouse Statistics Service!');
+});
+
+server.listen(port, function () {
+  var port = server.address().port;
+  console.log('App running on port ' + port);
+});
+
+
+
+
+
+
