b12476daad412c1c7f578d4ae4d53b127054897e
周文洋 <shuufumihiro@shuufumihiros-MacBook-Pro.local>
2020-02-06 17:22:50 +0800

    add axios

--- client/webNew/src/main.js
+++ client/webNew/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
