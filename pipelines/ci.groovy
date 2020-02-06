3521fed40bea7fbee47164eab445ba46a63d4f3a
Wilson Bo <wilsonbo@leansoftx.com>
2020-02-06 09:00:22 +0800

    change angent setting

--- pipelines/ci.groovy
+++ pipelines/ci.groovy
@@ -1,22 +1,40 @@
 pipeline {
-    agent 
-    { 
-        label 'vm-slave' 
-    } 
+    agent none
     stages {
         stage('Build') { 
+            agent 
+            { 
+                label 'vm-slave' 
+            } 
             steps {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']],
                             userRemoteConfigs: [[url: 'https://github.com/icdps/BoatHouse.git']]])
                 sh 'docker-compose -f docker-compose.yml -f docker-compose-standalone.yml up -d'
             }
         }
+        stage('Dev') { 
+            agent 
+            { 
+                label 'vm-slave' 
+            }
+            steps {
+                sh 'echo hello world! Dev!'
+            }
+        }
         stage('Test') { 
+            agent 
+            { 
+                label 'vm-slave' 
+            }
             steps {
                 sh 'echo hello world! Test!'
             }
         }
         stage('Deploy') { 
+            agent 
+            { 
+                label 'vm-slave' 
+            }
             steps {
                 sh 'echo hello world! Deploy!'
             }
