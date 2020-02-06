695c3f66a613b13fbbb6eb236630fba21758bd48
周文洋 <shuufumihiro@shuufumihiros-MacBook-Pro.local>
2020-02-06 22:51:58 +0800

    清理无效代码

--- client/web/src/router/index.js
+++ client/web/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Products from '@/components/Products'
-import Stores from '@/components/Stores'
 import Login from '@/components/Login'
 
 Vue.use(Router)
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'Products',
       component: Products
-    },
-    {
-      path: '/stores',
-      name: 'Stores',
-      component: Stores
     },
     {
       path: '/login',
