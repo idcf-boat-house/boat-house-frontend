998d2d83a3240027549f45e6f2763005519229a3
周文洋 <shuufumihiro@shuufumihiros-MacBook-Pro.local>
2020-02-07 20:32:54 +0800

    修复实时统计图标

--- managementNew/web/src/App.vue
+++ managementNew/web/src/App.vue
@@ -51,11 +51,15 @@
 
 <script>
 
+  var socket = io.connect({transports:['polling']});
+  var lineChart;
 
   export default {
     name: 'App',
     data () {
       return {
+        products: [],
+        productLike:[]
 
       }
     },
@@ -63,10 +67,152 @@
     },
     mounted(){
 
+      let _this=this;
+      var bg1 = document.getElementById('background-stats-1');
+      var bg2 = document.getElementById('background-stats-2');
+      //Get the context of the Chart canvas element we want to select
+      var ctx = $("#column-chart");
+
+      // Chart Options
+      var chartOptions = {
+        // Elements options apply to all of the options unless overridden in a dataset
+        // In this case, we are setting the border of each bar to be 2px wide and green
+        elements: {
+          rectangle: {
+            borderWidth: 2,
+            borderColor: 'rgb(0, 255, 0)',
+            borderSkipped: 'bottom'
+          }
+        },
+        responsive: true,
+        animation: false,
+        maintainAspectRatio: false,
+        responsiveAnimationDuration:500,
+        legend: {
+          display: false,
+          position: 'top',
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            gridLines: {
+              color: "#f3f3f3",
+              drawTicks: false,
+            },
+            scaleLabel: {
+              display: true,
+            },
+            ticks: {
+              fontSize: 14,
+              padding: 8
+            }
+          }],
+          yAxes: [{
+            display: true,
+            gridLines: {
+              color: "#f3f3f3",
+              drawTicks: false,
+            },
+            scaleLabel: {
+              display: true,
+            },
+            ticks: {
+              beginAtZero: true,
+              stepSize: 1
+            }
+          }]
+        },
+        title: {
+          display: false,
+          text: 'Chart.js Bar Chart'
+        }
+      };
+
+      // Chart Data
+      var chartData = {
+        labels: _this.products,
+        datasets: [{
+          label: "Like",
+          data: _this.productLike,
+          backgroundColor: "#28D094",
+          hoverBackgroundColor: "rgba(22,211,154,.9)",
+          borderColor: "transparent"
+        }]
+      };
+
+      var config = {
+        type: 'bar',
+
+        // Chart Options
+        options : chartOptions,
+
+        data : chartData
+      };
+
+      // Create the chart
+      lineChart = new Chart(ctx, config);
+
+
+      socket.on('message',function(data){
+        _this.init();
+      });
+
     },
     methods:{
+      init:function(){
+        document.body.style.opacity=1;
+        this.updateScores();
+      },
+      updateScores:function(){
+        let _this=this;
+        socket.on('scores', function (json) {
+          let data = JSON.parse(json);
+          var a = parseInt(data.a || 0);
+          var b = parseInt(data.b || 0);
+          var c = parseInt(data.c || 0);
+          var d = parseInt(data.d || 0);
+          var e = parseInt(data.e || 0);
+          var f = parseInt(data.f || 0);
 
-    }
+
+          var products=["蜂蜜烤猪肉","牛肉haggis","红酒炖羊腿","烟熏肠","蜂蜜猪肘","牛肉土豆泥"];
+          var productLike=[a,b,c,d,e,f];
+          _this.products=products;
+          _this.productLike=productLike;
+          
+          lineChart.data={
+            labels: products,
+            datasets: [{
+              label: "Like",
+              data: productLike,
+              backgroundColor:[
+                "rgba(255, 99, 132, 0.7)",
+                "rgba(255, 159, 64, 0.7)",
+                "rgba(255, 205, 86, 0.7)",
+                "rgba(75, 192, 192, 0.7)",
+                "rgba(54, 162, 235, 0.7)",
+                "rgba(153, 102, 255, 0.7)"
+              ],
+              borderColor:[
+                "rgb(255, 99, 132)",
+                "rgb(255, 159, 64)",
+                "rgb(255, 205, 86)",
+                "rgb(75, 192, 192)",
+                "rgb(54, 162, 235)",
+                "rgb(153, 102, 255)"
+              ],
+              hoverBackgroundColor: "rgba(22,211,154,.9)",
+              borderColor: "transparent"
+            }]
+          };
+          lineChart.update();
+
+
+
+        });
+      }
+
+  }
 
   }
 
