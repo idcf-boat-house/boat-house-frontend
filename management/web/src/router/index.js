7bf8f8307bd766af4434aae5a20a7d248e94accd
vincent wei <vincentwei@leansoftx.com>
2020-02-11 19:39:18 +0800

    管理端菜单添加菜品分类

--- management/web/src/router/index.js
+++ management/web/src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Dashboard from '@/components/Dashboard'
+import FoodCategory from '@/components/FoodCategory'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/FoodCategory',
+      name: 'FoodCategory',
+      component: FoodCategory
+    }
   ]
 })
