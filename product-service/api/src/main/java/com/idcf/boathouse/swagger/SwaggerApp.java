53cd34d7f2a774a7aafccb2831cacf9dcd0b2ab7
liaopengfei <liaopengfei@saicfinance.com>
2020-02-22 15:13:22 +0800

    修改swagger配置中package名称

--- product-service/api/src/main/java/com/idcf/boathouse/swagger/SwaggerApp.java
+++ product-service/api/src/main/java/com/idcf/boathouse/swagger/SwaggerApp.java
@@ -20,7 +20,7 @@ public class SwaggerApp {
                 .apiInfo(apiInfo())
                 .select()
                 //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.idcf.boathouse.Controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.idcf.boathouse.controller"))
                 .paths(PathSelectors.any())
                 .build();
 //        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
