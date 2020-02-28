db14043fc0f4107e3eee9cf1f8ec72a732598a70
jaylee <mr_lizhengjie@163.com>
2020-02-28 19:38:15 +0800

    增加mybatis-plus框架 增加建表语句和插入语句 个性springboot配置文件为yml格式 增加加盟新增和查询接口

---
+++ product-service/api/src/test/java/com/idcf/boathouse/mapper/JoinUsMapperTest.java
@@ -0,0 +1,27 @@
+package com.idcf.boathouse.mapper;
+
+import com.idcf.boathouse.models.JoinUs;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class JoinUsMapperTest {
+
+    @Resource
+    private JoinUsMapper joinUsMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<JoinUs> userList = joinUsMapper.selectList(null);
+        Assert.assertEquals(1, userList.size());
+        userList.forEach(System.out::println);
+    }
+}
