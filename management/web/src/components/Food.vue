a5c500d56f17bc3364a4e0680e49c31d104c5346
cqpaul <1547401163@qq.com>
2020-03-01 20:36:35 +0800

    修改为菜品管理

--- management/web/src/components/Food.vue
+++ management/web/src/components/Food.vue
@@ -3,7 +3,7 @@
     <div class="content-wrapper">
       <div class="content-header row">
         <div class="content-header-left col-md-6 col-12 mb-2 breadcrumb-new">
-          <h3 class="content-header-title mb-0 d-inline-block">菜品分类</h3>
+          <h3 class="content-header-title mb-0 d-inline-block">菜品管理</h3>
           <div class="row breadcrumbs-top d-inline-block">
             <div class="breadcrumb-wrapper col-12">
               <ol class="breadcrumb">
