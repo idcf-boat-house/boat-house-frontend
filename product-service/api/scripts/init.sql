f38876325f91d8d0d4a5fb787a8b547b06235bed
马杰 <1310069062@qq.com>
2020-03-04 23:16:00 +0800

    test only

--- product-service/api/scripts/init.sql
+++ product-service/api/scripts/init.sql
@@ -1,3 +1,16 @@
 create Database BoatHouse;
 use BoatHouse;
+DROP TABLE IF EXISTS `FoodCategory`;
 create table FoodCategory ( Id int auto_increment, Name nvarchar(50) null, Description nvarchar(1000) null, constraint FoodCategory_pk primary key (Id) );
+
+DROP TABLE IF EXISTS `intropage`;
+CREATE TABLE `intropage` (
+  `page_id` varchar(500) DEFAULT NULL,
+  `page_title` varchar(500) DEFAULT NULL,
+  `page_api_url` varchar(500) DEFAULT NULL,
+  `text` text,
+  `image` text,
+  `deleted` bit(1) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
