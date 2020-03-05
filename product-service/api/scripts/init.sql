d4c2f5b1bb270604680aaa089ef968de364adc25
Lynn Sun <sl5886639@126.com>
2020-03-05 10:43:15 +0800

    更改数据库建表语句 配置文件区分环境

--- product-service/api/scripts/init.sql
+++ product-service/api/scripts/init.sql
@@ -5,12 +5,13 @@ create table FoodCategory ( Id int auto_increment, Name nvarchar(50) null, Descr
 
 DROP TABLE IF EXISTS `intropage`;
 CREATE TABLE `intropage` (
-  `page_id` varchar(500) DEFAULT NULL,
+  `page_id` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `page_title` varchar(500) DEFAULT NULL,
   `page_api_url` varchar(500) DEFAULT NULL,
   `text` text,
   `image` text,
   `deleted` bit(1) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL
+  `update_time` datetime DEFAULT NULL,
+   PRIMARY KEY (`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
