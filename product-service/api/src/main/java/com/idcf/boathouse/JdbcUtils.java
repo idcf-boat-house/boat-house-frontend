9989fa45304d07330d98c0d3ae4bdffb15921e51
Lynn Sun <sl5886639@126.com>
2020-03-05 12:09:09 +0800

    更改数据库建表语句 更新jdbc逻辑

--- product-service/api/src/main/java/com/idcf/boathouse/JdbcUtils.java
+++ product-service/api/src/main/java/com/idcf/boathouse/JdbcUtils.java
@@ -1,5 +1,8 @@
 package com.idcf.boathouse;
 
+
+import org.springframework.beans.factory.annotation.Value;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -24,7 +27,20 @@ public class JdbcUtils {
     public JdbcUtils() {
         try {
         Properties pro = new Properties();
-        InputStream resource=this.getClass().getResourceAsStream("/application.properties");
+        InputStream resourceProperty=this.getClass().getResourceAsStream("/application.properties");
+        pro.load(resourceProperty);
+        String path="";
+        String profile=pro.getProperty("spring.profiles.active");;
+            if(profile.equals("dev")){
+                path="/application-dev.properties";
+            }
+            else if(profile.equals("test")){
+                path="/application-test.properties";
+            }
+            else {
+                path="/application-prod.properties";
+            }
+        InputStream resource=this.getClass().getResourceAsStream(path);
         pro.load(resource);
         URL = pro.getProperty("url");
         USERNAME = pro.getProperty("user");
