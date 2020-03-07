3a0fdb6139d3efcacfc97e6508c78c401be1fae9
JackyZhou <info@zhouwenyang.com>
2020-03-07 20:50:27 +0800

    update to 10s

--- statistics-service/worker/Program.cs
+++ statistics-service/worker/Program.cs
@@ -73,6 +73,7 @@ namespace Worker
             {
                 try
                 {
+                    Thread.Sleep(10000);
                     connection = new NpgsqlConnection(connectionString);
                     connection.Open();
                     break;
@@ -111,6 +112,7 @@ namespace Worker
             {
                 try
                 {
+                    Thread.Sleep(10000);
                     Console.Error.WriteLine("Connecting to redis");
                     return ConnectionMultiplexer.Connect(ipAddress);
                 }
