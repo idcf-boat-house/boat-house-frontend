dbd7670b64993986e498b2c90c03dcf602f8f347
tangzhiming <657345471@qq.com>
2020-03-08 19:40:41 +0800

    更新主库url地址

--- selenium/dotnet-uitest/UITest/src/TestBase.cs
+++ selenium/dotnet-uitest/UITest/src/TestBase.cs
@@ -21,7 +21,7 @@ namespace UITest
             this.output = output;
         }
 
-        public TestEnv env = TestEnv.Group;
+        public TestEnv env = TestEnv.MainRepo;
 
         private IWebDriver _driver;
 
@@ -36,7 +36,7 @@ namespace UITest
                     case TestEnv.Group:
                         return "d-docker.southeastasia.cloudapp.azure.com";
                     case TestEnv.MainRepo:
-                        return "";
+                        return "138.91.37.88";
                     default:
                         break;
                 }
