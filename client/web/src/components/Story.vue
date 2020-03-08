f6653b8e161463d326548a95df645de910189058
lykrsl <lykrsl@qq.com>
2020-03-08 15:02:46 +0800

    Story页面更新，添加了文字内容

--- client/web/src/components/Story.vue
+++ client/web/src/components/Story.vue
@@ -18,7 +18,7 @@
           <div class="left">
             <!-- text -->
             <div class="left-text">
-              这是一个美好的传说
+              18世纪90年代末的E国是一个战乱的国度，战争中，名为安德烈（Andre）的皇帝所带领的军队撤离到一个不知名的小镇，受伤的皇帝在这里遇上美丽典雅的牧场少女Aviva，Andre虽然受伤，但身为皇帝的Andre依然心系战事，希望能早日重返战场。
             </div>
             <!-- image -->
             <div class="left-img"></div>
@@ -27,7 +27,7 @@
           <div class="right">
             <!-- text -->
             <div class="right-text">
-              这是一方美味
+              Aviva明白他的想法，除了细心帮他料理伤口，还每天用石头加热烹制菜肴给他吃，因为当地人认为用石头加热烹制食物可以调理身体，对伤口的复原有帮助。后来安德烈（Andre）获得了胜利，为了怀念，Aviva在小镇上开起了一家名为Boat House的小餐馆，里面的菜式全部都是以石头加热烹制的各式菜肴。从此这个小镇改名为南皇后渡口（South Queensferry）。而Boat House餐馆成为了南皇后渡口小镇的标志性建筑。
             </div>
             <!-- image -->
             <div class="right-img"></div>
@@ -93,11 +93,13 @@ export default {
 .left-text{
   position:absolute;
   z-index:100;
-  background:#FF0;
-  width: 300px;
-  height:120px;
+  overflow-y:scroll;
+  background:#fff;
+  width: 350px;
+  height:200px;
   display:inline-block;
-  margin: 6rem 1rem 1rem 3rem;
+  margin: 2rem 1rem 1rem 3rem;
+  padding: 0.2rem;
   border: 5px solid rgb(255, 255, 255);
   border-radius: 1rem;
 }
@@ -105,12 +107,14 @@ export default {
 .right-text{
   position:absolute;
   z-index:100;
-  background:#FF0;
-  width: 300px;
-  height:120px;
+  overflow-y:scroll;
+  background:#fff;
+  width: 350px;
+  height:200px;
   bottom:0;
   display:inline-block;
-  margin: 1rem 1rem 8rem 8rem;
+  margin: 1rem 1rem 2rem 6rem;
+  padding: 0.2rem;
   border: 5px solid rgb(255, 255, 255);
   border-radius: 1rem;
 }
