5ac4076e55c5696b8d3fc98021003be4a803c1b5
谭彪 <tanb@51kingstone.com>
2020-03-23 17:35:40 +0800

    rm order code

--- management/web/src/router/index.js
+++ management/web/src/router/index.js
@@ -5,7 +5,6 @@ import FoodCategory from "@/components/FoodCategory";
 import Food from "@/components/Food";
 import UserMgt from "@/components/UserMgt";
 import JoinUs from "@/components/JoinUs";
-import Orders from "@/components/Orders";
 
 Vue.use(Router);
 
@@ -35,11 +34,6 @@ export default new Router({
       path: "/UserMgt",
       name: "UserMgt",
       component: UserMgt
-    },
-    {
-      path: "/Orders",
-      name: "Orders",
-      component: Orders
     }
   ]
 });
