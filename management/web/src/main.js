2af196472367e0c13b68766a0190a2e06946854a
jaylee <mr_lizhengjie@163.com>
2020-04-06 19:37:47 +0800

    合并冲突

--- management/web/src/main.js
+++ management/web/src/main.js
@@ -2,8 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ElementUI from 'element-ui' 
-import 'element-ui/lib/theme-chalk/index.css' 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
