2934cc50ac0621cb68886f6fb336bdeeddade35e
jasonchen <jasonchen@leansoftx.com>
2020-06-17 14:16:30 +0800

    update

--- src/product-service/api/src/main/java/com/idcf/boathouse/product/ProductApplication.java
+++ src/product-service/api/src/main/java/com/idcf/boathouse/product/ProductApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.idcf.boathouse.mapper")
+@MapperScan("com.idcf.boathouse.product.mapper")
 public class ProductApplication {
 
     public static void main(String[] args) {
